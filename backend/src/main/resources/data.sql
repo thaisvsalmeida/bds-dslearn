f8991a328cde0a79a7cb49f8af874a8417009ecd
Thaís Almeida <thaisvsalmeida@gmail.com>
2024-01-09 14:36:32 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('HTML', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
